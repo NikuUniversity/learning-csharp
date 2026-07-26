9d50fcc460f1b481a75546a457f9d674387e2028
Niku Univesity <nikuuniversity@gmail.com>
2026-07-26 19:51:09 +0530

    Class File Added

---
+++ Console Application/DemoConsoleApplication/DemoConsoleApplication/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoConsoleApplication
+{
+    //Car.cs - Represents a car with properties like Make, Model, and Year.
+    internal class Car
+    {
+    }
+}
---
+++ Console Application/DemoConsoleApplication/DemoConsoleApplication/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DemoConsoleApplication
+{
+    //Person.cs - Represents a person with properties like Name and Age.
+    internal class Person
+    {
+    }
+}
